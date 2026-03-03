6280a13c4661ecd581d992274443d79a4f443d77
Anshuman <anshumansr26@gmail.com>
2026-03-03 13:41:40 +0530

    03-03-2026

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,14 @@
 import express from "express";
 const app = express();
-app.listen(8080,()=>console.log("Server is running on port 8080"));
-app.get("/",(req,res)=>res.json({message:"Setup Success"}));
+app.listen(3030,()=>console.log("Server is running on port 3030"));
+
+// app level middleware
+import morgan from "morgan";
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// route level middleware
+import userRouter from "./user/user.routes.js";
+app.use("/api/user",userRouter);
---
+++ backend/src/user/user.controller.js
@@ -0,0 +1,18 @@
+export const createUser = async (req,res) => {
+    try{
+        const data = req.body;
+        console.log(data);
+        res.json({message:"Request Recieved"})
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+}
+
+export const login = async (req,res) => {
+    try{
+        const data = req.body;
+        console.log(data);
+    }catch(err){
+        res.status(500).json({message : err.message});
+    }
+}
